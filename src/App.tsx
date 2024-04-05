64f606e7db0aa1d456286a88601f2e1f61a878a1
Rubendvb <rubenvasquez556@gmail.com>
2024-04-05 18:44:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      Day DD Month MM Year YYYY -- years -- months -- days
+      <div className="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </>
+  )
+}
+
+export default App
