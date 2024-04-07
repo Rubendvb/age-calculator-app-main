f3a94afc3a874033ef30ef1974b6b83b4ad00d2f
Rubendvb <rubenvasquez556@gmail.com>
2024-04-06 23:50:16 -0300

    style mobile desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,82 @@
+import './App.css'
+
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
-      Day DD Month MM Year YYYY -- years -- months -- days
-      <div className="attribution">
+      <div className="card">
+        <header className="header">
+          <form className="form">
+            <div className="form__container">
+              <label className="form__label" htmlFor="day">
+                Day
+              </label>
+              <input
+                className="form__input"
+                type="number"
+                placeholder="DD"
+                id="day"
+                name="day"
+              />
+            </div>
+
+            <div className="form__container">
+              <label className="form__label" htmlFor="month">
+                Month
+              </label>
+              <input
+                className="form__input"
+                type="number"
+                placeholder="MM"
+                id="month"
+                name="month"
+              />
+            </div>
+
+            <div className="form__container">
+              <label className="form__label" htmlFor="year">
+                Year
+              </label>
+              <input
+                className="form__input"
+                type="number"
+                placeholder="YYYY"
+                id="year"
+                name="year"
+              />
+            </div>
+
+            <button className="form__button" type="submit">
+              <img src="/icon-arrow.svg" alt="" />
+            </button>
+          </form>
+        </header>
+
+        <main>
+          <div>
+            <p className="main__p">
+              <span>- -</span> years
+            </p>
+          </div>
+          <div>
+            <p className="main__p">
+              <span>- -</span> months
+            </p>
+          </div>
+          <div>
+            <p className="main__p">
+              <span>- -</span> days
+            </p>
+          </div>
+        </main>
+      </div>
+
+      <footer className="attribution">
         Challenge by{' '}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
+        . Coded by <a href="#">Rubén</a>.
+      </footer>
     </>
   )
 }
