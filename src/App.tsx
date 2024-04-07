b9bb841defb19aace9623bcbd6cd1337106a0f71
Rubendvb <rubenvasquez556@gmail.com>
2024-04-06 23:52:06 -0300

    component Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,12 @@
 import './App.css'
+import Form from './Form/Form'
 
 function App() {
   return (
     <>
       <div className="card">
         <header className="header">
-          <form className="form">
-            <div className="form__container">
-              <label className="form__label" htmlFor="day">
-                Day
-              </label>
-              <input
-                className="form__input"
-                type="number"
-                placeholder="DD"
-                id="day"
-                name="day"
-              />
-            </div>
-
-            <div className="form__container">
-              <label className="form__label" htmlFor="month">
-                Month
-              </label>
-              <input
-                className="form__input"
-                type="number"
-                placeholder="MM"
-                id="month"
-                name="month"
-              />
-            </div>
-
-            <div className="form__container">
-              <label className="form__label" htmlFor="year">
-                Year
-              </label>
-              <input
-                className="form__input"
-                type="number"
-                placeholder="YYYY"
-                id="year"
-                name="year"
-              />
-            </div>
-
-            <button className="form__button" type="submit">
-              <img src="/icon-arrow.svg" alt="" />
-            </button>
-          </form>
+          <Form />
         </header>
 
         <main>
